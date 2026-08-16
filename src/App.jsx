71e2d8262beae022bb4b223ae2d6fe4a8d4b6b0c
name <gafarova.tasia@gmail.com>
2026-08-16 07:52:10 +0300

    add routes, showMapBuilder error

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import './App.css'
 import Game from './components/./Game.jsx'
 import MapBuilder from "./components/MapBuilder.jsx";
 import {useState} from "react";
+import {NavLink, Outlet} from "react-router-dom";
+
 function App() {
     const [showMapBuilder, setShowMapBuilder] = useState(false)
 
     return (
         <>
-
-            <button onClick={() => setShowMapBuilder(!showMapBuilder)} style={{color: '#4a90e2', margin: '5px'}}>Change map</button>
-            {showMapBuilder ? <MapBuilder /> : <Game />}
+            <button onClick={() => setShowMapBuilder(!showMapBuilder)} style={{color: '#4a90e2', margin: '5px'}}>Change
+                map
+            </button>
+            {showMapBuilder ? <MapBuilder/> : <Game/>}
+            <nav>
+                <NavLink to='/'>Main</NavLink>
+                <NavLink to='/mapBuilder'>MapBuilder</NavLink>
+            </nav>
+            <hr/>
+            <Outlet/>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Game from "./components/Game.jsx";
+import MapBuilder from "./components/MapBuilder.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*<App />*/}
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" component={App} />
+            {/*<Route path="/" element={<App />} />*/}
+            <Route index element={<Game />} />
+            <Route path='/mapBuilder' component={MapBuilder} />
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
